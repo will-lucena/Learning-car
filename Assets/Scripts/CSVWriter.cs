33d56b5bb8cc39d2ecbb1fb00eab7614b4a123b5
Will <will.lucen4@gmail.com>
2019-11-21 15:40:05 -0300

    First data generation, waiting validation

---
+++ Assets/Scripts/CSVWriter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class CSVWriter : MonoBehaviour
+{
+    public static System.Func<char> requestSeparator;
+
+    private char separator;
+    private int filesGenerated = 5;
+    // Use this for initialization
+    void Start ()
+    {
+        DataCollection.provideData += save;
+        separator = (char) requestSeparator?.Invoke();
+    }
+    
+    void save(List<string> data)
+    {
+        string header = "sensor-left" + separator + "sensor-front" + separator + "sensor-right" + separator + "speed";
+        data.Insert(0, header);
+        string filePath = getPath();
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(header);
+        foreach (var line in data)
+        {
+            sb.AppendLine(line);
+        }
+        
+        StreamWriter outStream = System.IO.File.CreateText(filePath);
+        outStream.WriteLine(sb.ToString());
+        outStream.Close();
+        Debug.Log(filePath);
+        filesGenerated++;
+    }
+
+    // Following method is used to retrive the relative path as device platform
+    private string getPath(){
+        return Application.dataPath +"/csv/"+"dataset" + filesGenerated + ".csv";
+    }
+}
---
+++ Assets/Scripts/DataCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollection : MonoBehaviour
+{
+    public static Action<List<string>> provideData;
+    
+    public Transform sensorOrigin;
+    public char separator;
+    public int batchSize;
+
+    private List<string> data;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        CSVWriter.requestSeparator += () => separator;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        data = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+        
+        RaycastHit leftSensor;
+        RaycastHit frontSensor;
+        RaycastHit rightSensor;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(sensorOrigin.position, transform.TransformDirection(Vector3.left), out leftSensor, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(sensorOrigin.position, transform.TransformDirection(Vector3.left) * leftSensor.distance, Color.yellow);
+        }
+        
+        if (Physics.Raycast(sensorOrigin.position, transform.TransformDirection(Vector3.forward), out frontSensor, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(sensorOrigin.position, transform.TransformDirection(Vector3.forward) * frontSensor.distance, Color.yellow); ;
+        }
+        
+        if (Physics.Raycast(sensorOrigin.position, transform.TransformDirection(Vector3.right), out rightSensor, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(sensorOrigin.position, transform.TransformDirection(Vector3.right) * rightSensor.distance, Color.yellow);
+        }
+
+        string sensorOutput = leftSensor.distance.ToString() + separator + frontSensor.distance + separator +
+                              rightSensor.distance + separator + rb.velocity.magnitude;
+        data.Add(sensorOutput);
+        
+        if (data.Count >= batchSize)
+        {
+            provideData?.Invoke(data);
+            data.Clear();
+        }
+    }
+}
