5535d925625ee1426a4e30242d88369bfa6e0223
Paulo Aragão <33064367+paulocosmico@users.noreply.github.com>
2019-11-27 20:47:04 -0300

    fixed data files and UI

--- Assets/Scripts/DataCrawler.cs
+++ Assets/Scripts/DataCrawler.cs
@@ -25,21 +25,35 @@ public class DataCrawler : MonoBehaviour
     private TextMeshProUGUI sensorRightUI;
     [SerializeField]
     private TextMeshProUGUI sensorFrontUI;
+    [SerializeField]
+    private Slider generatingUI;
 
     private void Awake()
     {
         CSVWriter.requestSeparator += () => separator;
         rb = GetComponent<Rigidbody>();
+
     }
 
     private void Start()
     {
         data = new List<string>();
+        generatingUI.value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //UI slider controller, percent of writer csv file
+        if (generatingUI.value < 1000)
+        {
+            generatingUI.value += 1;
+        }
+        else
+        {
+            generatingUI.value = 0;
+        }
+
         // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 8;
 
@@ -54,6 +68,7 @@ public class DataCrawler : MonoBehaviour
         if (Physics.Raycast(sensorOrigin.position, transform.TransformDirection(Vector3.left), out leftSensor, Mathf.Infinity, layerMask))
         {
             Debug.DrawRay(sensorOrigin.position, transform.TransformDirection(Vector3.left) * leftSensor.distance, Color.yellow);
+            
         }
         
         if (Physics.Raycast(sensorOrigin.position, transform.TransformDirection(Vector3.forward), out frontSensor, Mathf.Infinity, layerMask))
@@ -76,26 +91,23 @@ public class DataCrawler : MonoBehaviour
         {
             directionUI.text = "<<<";
             directionOutput = -1;
-            directionUI.color = sensorLeftUI.color;
         }else if (directionOutput > 0.1)
         {
             directionUI.text = ">>>";
             directionOutput = 1;
-            directionUI.color = sensorRightUI.color;
         }
         else
         {
             directionUI.text = "^";
             directionOutput = 0;
-            directionUI.color = new Color(255,255,255);
         }
         
         sensorFrontUI.text = frontSensor.distance.ToString();
         sensorLeftUI.text = leftSensor.distance.ToString();
         sensorRightUI.text = rightSensor.distance.ToString();
 
-        string sensorOutput = leftSensor.distance.ToString() + separator + frontSensor.distance + separator +
-                              rightSensor.distance + separator + directionOutput;
+        string sensorOutput = (leftSensor.distance.ToString()+ separator + frontSensor.distance + separator +
+                              rightSensor.distance + separator + directionOutput).Replace(',', '.');
         data.Add(sensorOutput);
 
         if (data.Count >= batchSize)
